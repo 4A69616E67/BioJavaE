d8b19b9e0038b3abfbbfe497322055f9227ff594
HaoJiang <john-jh@foxmail.com>
2021-01-14 16:01:19 +0800

    FastqFile modify

--- src/com/github/SnowFlakes/File/FastQFile/FastqFile.java
+++ src/com/github/SnowFlakes/File/FastQFile/FastqFile.java
@@ -1,8 +1,9 @@
 package com.github.SnowFlakes.File.FastQFile;
 
-import com.github.SnowFlakes.File.AbstractFile;
+import com.github.SnowFlakes.IO.FastqReaderExtension;
+import com.github.SnowFlakes.IO.FastqWriterExtension;
 import com.github.SnowFlakes.tool.Tools;
-import com.github.SnowFlakes.unit.*;
+import htsjdk.samtools.fastq.FastqRecord;
 
 import java.io.*;
 import java.util.*;
@@ -10,133 +11,97 @@ import java.util.*;
 /**
  * Created by snowf on 2019/2/17.
  */
-public class FastqFile extends AbstractFile<FastqItem> {
+public class FastqFile extends File {
+    enum QualityFormat {
+        PHRED33, PHRED64
+    }
 
     public FastqFile(File file) {
-        super(file);
+        super(file.getPath());
     }
 
     public FastqFile(String s) {
-        this(new File(s));
+        super(s);
     }
 
     public FastqFile(FastqFile file) {
-        super(file);
+        super(file.getPath());
     }
 
-    @Override
-    protected FastqItem ExtractItem(String[] s) {
-        FastqItem Item;
-        if (s == null) {
-            Item = null;
-        } else {
-            Item = new FastqItem(s[0]);
-            Item.Sequence = s[1];
-            Item.Orientation = s[2];
-            Item.Quality = s[3];
-        }
-        return Item;
-    }
 
-    @Override
-    public synchronized String[] ReadItemLine() throws IOException {
-        String[] s = new String[4];
-        for (int i = 1; i <= 4; i++) {
-            String line = reader.readLine();
-            if (line != null) {
-                s[i - 1] = line;
-            } else {
-                return null;
-            }
-        }
-        return s;
-    }
-
-    @Override
-    public void WriteItem(FastqItem item) throws IOException {
-        writer.write(item.toString());
-    }
-
-    public Opts.FileFormat FastqPhred() throws IOException {
-        FastqItem Item;
-        ReadOpen();
-        int[] FormatEdge = new int[] { (int) '9', (int) 'K' };
+    public static QualityFormat FastqPhred(FastqFile file) throws IOException {
+        FastqReaderExtension reader = new FastqReaderExtension(file);
+        FastqRecord Item;
+        int[] FormatEdge = new int[]{(int) '9', (int) 'K'};
         int[] Count = new int[2];
         int LineNum = 0;
-        while ((Item = ReadItem()) != null && ++LineNum <= 100) {
-            for (int i = 0; i < Item.Quality.length(); i++) {
-                if ((int) Item.Quality.charAt(i) <= FormatEdge[0]) {
+        while ((Item = reader.ReadRecode()) != null && ++LineNum <= 100) {
+            for (byte q : Item.getBaseQualities()) {
+                if (q <= FormatEdge[0]) {
                     Count[0]++;
-                } else if ((int) Item.Quality.charAt(i) >= FormatEdge[1]) {
+                } else if (q >= FormatEdge[1]) {
                     Count[1]++;
                 }
             }
         }
-        ReadClose();
-        return Count[0] >= Count[1] ? Opts.FileFormat.Phred33 : Opts.FileFormat.Phred64;
+        reader.close();
+        return Count[0] >= Count[1] ? QualityFormat.PHRED33 : QualityFormat.PHRED64;
     }
 
-    public ArrayList<FastqItem> ExtractID(Collection<String> List) throws IOException {
+    public ArrayList<FastqRecord> ExtractID(Collection<String> List) throws IOException {
         return ExtractID(List, 1);
     }
 
-    public ArrayList<FastqItem> ExtractID(Collection<String> List, int threads) throws IOException {
-        ArrayList<FastqItem> ResList = new ArrayList<>();
-        ReadOpen();
+    public ArrayList<FastqRecord> ExtractID(Collection<String> List, int threads) throws IOException {
+        ArrayList<FastqRecord> ResList = new ArrayList<>();
+        FastqReaderExtension reader = new FastqReaderExtension(this);
         Thread[] t = new Thread[threads];
         for (int i = 0; i < t.length; i++) {
             t[i] = new Thread(() -> {
-                FastqItem item;
-                try {
-                    while ((item = ReadItem()) != null) {
-                        if (List.contains(item.Title)) {
-                            synchronized (this) {
-                                ResList.add(item);
-                                List.remove(item.Title);
-                                if (List.size() <= 0) {
-                                    break;
-                                }
+                FastqRecord item;
+                while ((item = reader.ReadRecode()) != null) {
+                    if (List.contains(item.getReadName())) {
+                        synchronized (this) {
+                            ResList.add(item);
+                            List.remove(item.getReadName());
+                            if (List.size() <= 0) {
+                                break;
                             }
                         }
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
             });
             t[i].start();
         }
         Tools.ThreadsWait(t);
-        ReadClose();
+        reader.close();
         return ResList;
     }
 
     public void ExtractID(Collection<String> List, int threads, FastqFile OutFile) throws IOException {
-        ReadOpen();
-        OutFile.WriteOpen();
+        FastqReaderExtension reader = new FastqReaderExtension(this);
+        FastqWriterExtension writer = new FastqWriterExtension(OutFile);
         Thread[] t = new Thread[threads];
         for (int i = 0; i < t.length; i++) {
             t[i] = new Thread(() -> {
-                FastqItem item;
-                try {
-                    while ((item = ReadItem()) != null) {
-                        if (List.contains(item.Title)) {
-                            synchronized (this) {
-                                OutFile.WriteItemln(item);
-                                List.remove(item.Title);
-                                if (List.size() <= 0) {
-                                    break;
-                                }
+                FastqRecord item;
+                while ((item = reader.ReadRecode()) != null) {
+                    if (List.contains(item.getReadName())) {
+                        synchronized (this) {
+                            writer.WriterRecodeln(item);
+                            List.remove(item.getReadName());
+                            if (List.size() <= 0) {
+                                break;
                             }
                         }
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
+
             });
             t[i].start();
         }
         Tools.ThreadsWait(t);
-        ReadClose();
-        OutFile.WriteClose();
+        reader.close();
+        writer.close();
     }
 }
